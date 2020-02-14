f15d9aae465e0d964c037650734193b671f0eb66
Eddie Madrigal <edmadrigal@yahoo.com>
2020-02-14 10:04:03 -0800

    articles complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,67 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function Cards(data) {
+    // create elements
+    const divCard = document.createElement('div');
+    const divHeadline = document.createElement('div');
+    const divAuthor = document.createElement('div');
+    const divImageContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
+
+    // create structure
+    divCard.append(divHeadline, divAuthor);
+    divAuthor.append(divImageContainer, span);
+    divImageContainer.append(img)
+
+    // add classes
+    divCard.classList.add('card');
+    divHeadline.classList.add('headline');
+    divImageContainer.classList.add('img-container');
+
+    // add data
+    divHeadline.textContent = data.headline;
+    img.src = data.authorPhoto;
+    span.innerHTML = `By ${data.authorName}`;
+
+    // add attributes
+    img.setAttribute('style', 'width: 200px;');
+
+    return divCard;
+}
+
+const entryPoint = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        response.data.articles.javascript.map(item => {
+            entryPoint.append(Cards(item));
+        })
+        return response;
+    })
+    .then(response => {
+        response.data.articles.bootstrap.map(item => {
+            entryPoint.append(Cards(item));
+        })
+        return response;
+    })
+    .then(response => {
+        response.data.articles.technology.map(item => {
+            entryPoint.append(Cards(item));
+        })
+        return response;
+    })
+    .then(response => {
+        response.data.articles.jquery.map(item => {
+            entryPoint.append(Cards(item));
+        })
+        return response;
+    })
+    .then(response => {
+        response.data.articles.node.map(item => {
+            entryPoint.append(Cards(item));
+        })
+    })
+    
